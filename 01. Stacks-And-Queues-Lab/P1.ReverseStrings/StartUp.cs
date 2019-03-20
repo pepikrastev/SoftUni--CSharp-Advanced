fad89593833a52267566e8970a706e72918b142d
the_freestyle <the_freestyle@abv.bg>
2019-03-20 17:51:00 +0200

    update

---
+++ 01. Stacks-And-Queues-Lab/P1.ReverseStrings/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+
+        Stack<char> text = new Stack<char>();
+
+        string input = Console.ReadLine();
+
+        foreach (char item in input)
+        {
+            char ch = item;
+            text.Push(ch);
+        }
+
+        while (text.Count != 0)
+        {
+            Console.Write(text.Pop());
+        }
+        Console.WriteLine();
+    }
+}
---
+++ 01. Stacks-And-Queues-Lab/P5.Supermarket/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class StartUp
+{
+    static void Main(string[] args)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        Queue<string> names = new Queue<string>();
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            if (input == "End")
+            {
+                break;
+            }
+            else if (input == "Paid")
+            {
+                while (names.Count > 0)
+                {
+                    stringBuilder.AppendLine(names.Dequeue());
+                }
+            }
+            else
+            {
+                names.Enqueue(input);
+            }
+        }
+
+        stringBuilder.AppendLine($"{names.Count} people remaining.");
+
+        Console.Write(stringBuilder);
+    }
+}
