d1bf6025bea97014f93ee20267196c0799c31487
the_freestyle <the_freestyle@abv.bg>
2019-04-21 12:56:21 +0300

    update

---
+++ 06.Generics - Lab/P01.Box/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+   public class Box<T>
+    {
+        Stack<T> list;
+
+
+        public Box()
+        {
+            this.list = new Stack<T>();
+        }
+
+        public int Count { get; private  set; }
+
+        public void Add(T element)
+        {
+            list.Push(element);
+            this.Count++;
+        }
+
+        public T Remove()
+        {
+            this.Count--;
+            return list.Pop();
+        }
+    }
+}
---
+++ 06.Generics - Lab/P03.GenericScale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+    
+
