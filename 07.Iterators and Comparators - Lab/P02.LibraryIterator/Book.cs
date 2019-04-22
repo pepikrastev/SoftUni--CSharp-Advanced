84f55b1fe977e32837ca8e1e4ed6c4216c1bebb0
the_freestyle <the_freestyle@abv.bg>
2019-04-22 14:56:53 +0300

    update

---
+++ 07.Iterators and Comparators - Lab/P02.LibraryIterator/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+        public Book(string title, int year, params string[] author)
+        {
+            Title = title;
+            Year = year;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+       public IReadOnlyList<string> Author { get; set; }
+    }
+}
---
+++ 07.Iterators and Comparators - Lab/P04.BookComparer/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P04.BookComparer
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
